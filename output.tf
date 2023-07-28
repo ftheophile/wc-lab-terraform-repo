b5cf38386af51711b36838c7f73866f7cbe75db9
Theophile Fondzefe <ftheophile.fondzefe@gmail.com>
2023-07-28 23:45:42 +0200

    Added outputs

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "instance_elastic_ip" {
+    value = aws_eip.one.public_ip
+}
+
+output "instance_private_ip" {
+    value = aws_instance.my-first-server.private_ip
+}
+
+output "first_server_state" {
+    value = aws_instance.my-first-server.instance_state
+}
